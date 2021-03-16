94d8fe84acffbd0aa6ee3634c492b6ef3d91793d
Dhdndir <51747337+Dhdndir@users.noreply.github.com>
2021-03-16 15:42:14 +0800

    Update iOS天气.js

--- iOS天气.js
+++ iOS天气.js
@@ -6,7 +6,7 @@ const aqicnToken = '3710d3a0be54d623a8a343c6a4b80f76457b8423'
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
+	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=https://raw.githubusercontent.com/Dhdndir/-x-/main/iOS天气.js
 	[MITM]
 	hostname = weather-data.apple.com
 */
